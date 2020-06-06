31321cbb0b24b5db565d65de4e0d0682221af690
수진 <sujin9743@users.noreply.github.com>
2020-06-07 01:15:23 +0900

    signcomplete xml complete

---
+++ app/src/main/java/jsj/mjc/hobbybook/SignCompleteActivity.java
@@ -0,0 +1,14 @@
+package jsj.mjc.hobbybook;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignCompleteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_complete);
+    }
+}
