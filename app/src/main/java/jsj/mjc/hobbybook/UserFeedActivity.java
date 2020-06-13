4199d905a2b6a86a6a170650aeda6d10eb713678
수진 <sujin9743@users.noreply.github.com>
2020-06-14 04:39:26 +0900

    사용자피드 xml 완성

---
+++ app/src/main/java/jsj/mjc/hobbybook/UserFeedActivity.java
@@ -0,0 +1,49 @@
+package jsj.mjc.hobbybook;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_feed);
+
+
+        //툴바 설정
+        Toolbar userFeed_toolbar = (Toolbar) findViewById(R.id.userFeed_toolbar);
+        setSupportActionBar(userFeed_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_chevron_left_black_24dp);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.userfeedtoolbar, menu);
+        return true;
+    }
+
+    //툴바 오버플로우 메뉴 이벤트 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.overflow_report: {
+            }
+            case R.id.overflow_block: {
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
