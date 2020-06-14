c95d263ec901702f3a552288d98c817dab8343c9
수진 <sujin9743@users.noreply.github.com>
2020-06-14 23:16:40 +0900

    피드 독후감 리사이클러뷰 어댑터 작성 중

--- app/src/main/java/jsj/mjc/hobbybook/FeedReadBookAdapter.java
+++ app/src/main/java/jsj/mjc/hobbybook/FeedReadBookAdapter.java
@@ -1,5 +1,49 @@
 package jsj.mjc.hobbybook;
 
-import java.util.ArrayList;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+public class FeedReadBookAdapter extends RecyclerView.Adapter<FeedReadBookAdapter.FeedReadBookViewHolder> {
+
+    private ArrayList<FeedReadBookItem> readbooklist;
+
+    public class FeedReadBookViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView bookCoverImg;
+
+        public FeedReadBookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookCoverImg = itemView.findViewById(R.id.bookcoverImg);
+        }
+    }
+
+    //데이터 리스트 객체 전달
+    FeedReadBookAdapter(ArrayList<FeedReadBookItem> readbooklist) {
+        this.readbooklist = readbooklist;
+    }
+
+    @NonNull
+    @Override
+    public FeedReadBookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedReadBookViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
