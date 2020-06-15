fa7b3a85c206d4b8be249f53d39e9a821b0e3581
Jieun1012 <vnfmsskfro99>
2020-06-15 18:52:37 +0900

    화면 이동(메인화면에서 도서 추천, 토론게시판) 추가

--- app/src/main/java/jsj/mjc/hobbybook/MessageViewActivity.java
+++ app/src/main/java/jsj/mjc/hobbybook/MessageViewActivity.java
@@ -1,11 +1,15 @@
 package jsj.mjc.hobbybook;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MessageViewActivity extends AppCompatActivity {
     @Override
@@ -13,13 +17,35 @@ public class MessageViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_view);
 
-        ImageButton mVeiw_back_btn = findViewById(R.id.mView_back_btn);
+        //툴바 설정
+        Toolbar userFeed_toolbar = (Toolbar) findViewById(R.id.mView_toolbar);
+        setSupportActionBar(userFeed_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_chevron_left_black_24dp);
+    }
 
-        mVeiw_back_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.messageoverflow, menu);
+        return true;
+    }
+
+    //툴바 오버플로우 메뉴 이벤트 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 finish();
-            }
-        });
+                break;
+            case R.id.overflow_report:
+                ReportDialog reportDialog = new ReportDialog(MessageViewActivity.this);
+                reportDialog.show();
+                break;
+            case R.id.overflow_block:
+            case R.id.overflow_delete:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
