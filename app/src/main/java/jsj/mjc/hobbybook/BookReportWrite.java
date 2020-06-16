deda88c0b552fd26fd97ed442b41bf448986d3ec
goatmxj980111 <46188825+goatmxj980111@users.noreply.github.com>
2020-06-16 21:59:42 +0900

    유월 16일 조민주 커뮛7

--- app/src/main/java/jsj/mjc/hobbybook/BookReportWrite.java
+++ app/src/main/java/jsj/mjc/hobbybook/BookReportWrite.java
@@ -1,13 +1,26 @@
 package jsj.mjc.hobbybook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BookReportWrite extends AppCompatActivity {
+    ImageView backBtn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_report_write);
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 
