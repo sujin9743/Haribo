f61eb62a73eaf5f36047cfd600c9fa9e68f4aa9b
goatmxj980111 <46188825+goatmxj980111@users.noreply.github.com>
2020-06-16 21:56:43 +0900

    유월 16일 조민주 커뮛6

--- app/src/main/java/jsj/mjc/hobbybook/MBookComment.java
+++ app/src/main/java/jsj/mjc/hobbybook/MBookComment.java
@@ -1,14 +1,26 @@
 package jsj.mjc.hobbybook;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MBookComment extends AppCompatActivity {
 
+    ImageView backBtn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.m_book_comment);
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 
--- app/src/main/java/jsj/mjc/hobbybook/MBookInfoDetail.java
+++ app/src/main/java/jsj/mjc/hobbybook/MBookInfoDetail.java
@@ -1,14 +1,36 @@
 package jsj.mjc.hobbybook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MBookInfoDetail extends AppCompatActivity {
 
+    ImageView backBtn;
+    LinearLayout letsGoReport;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_info_detail);
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        letsGoReport = findViewById(R.id.letsGoReport);
+        letsGoReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MBookReportDetail.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
