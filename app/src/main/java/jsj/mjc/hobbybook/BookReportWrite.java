aa59ab4a020cbb2230f50f26ff845f2efa151729
goatmxj980111 <46188825+goatmxj980111@users.noreply.github.com>
2020-06-16 20:21:10 +0900

    유월 16일 조민주 커뮛

---
+++ app/src/main/java/jsj/mjc/hobbybook/BookReportWrite.java
@@ -0,0 +1,14 @@
+package jsj.mjc.hobbybook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookReportWrite extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_report_write);
+    }
+
+
+}
---
+++ app/src/main/java/jsj/mjc/hobbybook/MRealtimeBookReport.java
@@ -0,0 +1,70 @@
+package jsj.mjc.hobbybook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MRealtimeBookReport extends AppCompatActivity {
+
+
+    ImageView backBtn, serchBtn,Heart,addBookReport;
+    CircleImageView profileImg;
+    TextView profileText, bookName, bookCreator, likeCount, commentCnt;
+    ViewPager bookImgPage;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.realtime_book_report);
+
+        backBtn = findViewById(R.id.backBtn);
+        serchBtn = findViewById(R.id.serchBtn);
+        profileImg = findViewById(R.id.profileImg);
+        profileText = findViewById(R.id.profileText);
+        addBookReport = findViewById(R.id.addBookReport);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class );
+                startActivity(intent);
+            }
+        });
+        serchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MRealtimeBookReport.this, MRealtimeBookReportSearch.class);
+                startActivityForResult(intent,1);
+            }
+        });
+        profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),UserFeedActivity.class);
+                startActivity(i);
+            }
+        });
+        profileText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),UserFeedActivity.class);
+                startActivity(i);
+            }
+        });
+        addBookReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),BookReportWrite.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
