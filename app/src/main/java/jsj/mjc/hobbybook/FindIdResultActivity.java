35d61b1476bd795388f9931db74aba831d914930
수진 <sujin9743@users.noreply.github.com>
2020-11-14 23:44:13 +0900

    수진-도서추천 서버연동(장르 여러 개 선택했을 때),SignActivity, SelectGenreActivity 삭제

--- app/src/main/java/jsj/mjc/hobbybook/FindIdResultActivity.java
+++ app/src/main/java/jsj/mjc/hobbybook/FindIdResultActivity.java
@@ -1,7 +1,9 @@
 package jsj.mjc.hobbybook;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -44,6 +46,7 @@ public class FindIdResultActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
+
         });
     }
 }
