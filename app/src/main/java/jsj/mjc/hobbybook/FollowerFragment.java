5992c506520fea4552417ebdd6743d9d0b854a99
Jieun1012 <vnfmsskfro99>
2020-11-17 03:41:06 +0900

    팔로잉 팔로워 일부 구현(정창 오빠 완료 후 나머지 가능)

--- app/src/main/java/jsj/mjc/hobbybook/FollowerFragment.java
+++ app/src/main/java/jsj/mjc/hobbybook/FollowerFragment.java
@@ -2,6 +2,7 @@ package jsj.mjc.hobbybook;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +16,25 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.StorageReference;
+
 public class FollowerFragment extends Fragment {
     ArrayList<UserlistItem> userlist;
     UserListAdapter userListAdapter;
+    String loginId, userId;
+    final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    StorageReference storageRef;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_follower, container, false);
+        loginId = getActivity().getIntent().getStringExtra("loginId");
+        userId = getActivity().getIntent().getStringExtra("userId");
 
         //RecyclerView
         userlist = new ArrayList<>();
@@ -34,21 +47,32 @@ public class FollowerFragment extends Fragment {
 
         //recyclerView 구분선 추가
         follower_recycler.addItemDecoration(new DividerItemDecoration(follower_recycler.getContext(), 1));
-
-        //임시 데이터 삽입
-        for(int i=0; i<20; i++) {
-            //todo . 내가 팔로우하지 않은 사람만 팔로우 버튼 뜨게
-            UserlistItem data = new UserlistItem("하리보", "팔로우");
-            userlist.add(data);
-        }
         follower_recycler.setAdapter(userListAdapter);
 
-        //RecyclerView 항목 클릭 구현
-        userListAdapter.setOnItemClickListener(new UserListAdapter.OnItemClickListenr() {
+        //팔로잉 로드
+        db.collection("follow").whereEqualTo("followee", userId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onItemClick(View v, int position) {
-                Intent intent = new Intent(getContext(), UserFeedActivity.class);
-                startActivity(intent);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //TODO 사용자 추천과 겹치니 나중에
+                        UserlistItem data = new UserlistItem(document.getString("follower"), "팔로우");
+                        userlist.add(data);
+                        //RecyclerView 항목 클릭 구현
+                        userListAdapter.setOnItemClickListener(new UserListAdapter.OnItemClickListenr() {
+                            @Override
+                            public void onItemClick(View v, int position) {
+                                Intent intent = new Intent(getContext(), UserFeedActivity.class);
+                                intent.putExtra("loginId", loginId);
+                                intent.putExtra("userId", loginId);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                    userListAdapter.notifyDataSetChanged();
+                } else {
+                    Log.d("lll", "팔로잉 로드 오류 : ", task.getException());
+                }
             }
         });
 
