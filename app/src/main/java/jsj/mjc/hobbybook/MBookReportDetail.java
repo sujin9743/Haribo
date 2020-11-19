b3b797617a5eafeaa36bb8abd4cab9b47efe0bd2
goatmxj980111 <46188825+goatmxj980111@users.noreply.github.com>
2020-11-19 21:38:31 +0900

    실시간 독후감 > 독후감 상세페이지 닉네임, 프로필사진

--- app/src/main/java/jsj/mjc/hobbybook/MBookReportDetail.java
+++ app/src/main/java/jsj/mjc/hobbybook/MBookReportDetail.java
@@ -143,7 +143,7 @@ public class MBookReportDetail extends AppCompatActivity {
                             open = document.getBoolean("open");
                         }
                     });
-            
+
 
 
         } else {
